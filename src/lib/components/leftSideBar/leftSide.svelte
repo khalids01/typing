14b149ea8d478e88112adf4dd82d9a5bb731046b
khalids01 <khalids8774@gmail.com>
2023-06-20 03:22:07 +0600

    sidebars start

---
+++ src/lib/components/leftSideBar/leftSide.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { SlideToggle, ListBox, ListBoxItem } from "@skeletonlabs/skeleton";
+  import Flex from "../ui/Flex.svelte";
+
+  const settings = {
+    story: {
+      value: true,
+      label: "Story",
+    },
+    capitalLetter: {
+      value: true,
+      label: "Capital Letter",
+    },
+    punctuation: {
+      value: true,
+      label: "Punctuation",
+    },
+    keyboard: {
+      value: true,
+      label: "Keyboard",
+    },
+  };
+</script>
+
+<div class="px-3 py-4 min-w-[200px] flex flex-col items-stretch gap-4">
+  {#each Object.entries(settings) as [key, option]}
+    <div class="flex justify-between items-center gap-3 ">
+      <label class="font-medium text-sm" for={key}>{option.label}</label>
+      <div class="toggler inline-flex justify-end items-center">
+        <SlideToggle
+          style={`transform: scale(0.8);`}
+          name={option.label}
+          bind:checked={option.value}
+          active="bg-secondary-500"
+          size="sm"
+          id={key}
+        />
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .toggler {
+    transform: scale(0.8) !important;
+  }
+</style>
--- src/lib/components/ui/Flex.svelte
+++ src/lib/components/ui/Flex.svelte
@@ -1,35 +1,38 @@
 <script>
-	export let justify = 'flex-start';
-	export let align = 'flex-start';
-	export let wrap = 'no-wrap';
-	export let dir = 'row';
-	export let h = 'max-content';
-	export let w = 'max-content';
-	export let cls = 'flex-element ';
+  export let justify = "flex-start";
+  export let align = "flex-start";
+  export let wrap = "no-wrap";
+  export let dir = "row";
+  export let h = "max-content";
+  export let w = "max-content";
+  export let cls = "flex-element ";
+  export let gap = "0px";
 </script>
 
 <div
-	class={cls}
-	style={`
+  class={cls}
+  style={`
 --justify: ${justify};
 --align: ${align};
 --wrap: ${wrap};
 --dir: ${dir};
 --h: ${h},
 --w: ${w},
+--gap: ${gap}
 `}
 >
-	<slot />
+  <slot />
 </div>
 
 <style>
-	div {
-		display: flex !important;
-		justify-content: var(--justify);
-		align-items: var(--align);
-		flex-wrap: var(--wrap);
-		flex-direction: var(--dir);
-		height: var(--h);
-		width: var(--w);
-	}
+  div {
+    display: flex !important;
+    justify-content: var(--justify);
+    align-items: var(--align);
+    flex-wrap: var(--wrap);
+    flex-direction: var(--dir);
+    height: var(--h);
+    width: var(--w);
+    gap: var(--gap);
+  }
 </style>
